470e8d6040166e5230f56c312fde1a2996c694a3
AlenaNik <alena.nik.digital@gmail.com>
2020-02-18 21:01:00 +0100

    route params

---
+++ pages/blogs/_post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ slug.post }}</h1>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data(){
+      return {
+        slug: this.$route.params
+      }
+    },
+    created() {
+      // console.log(this.$route.params)
+    }
+  }
+</script>
---
+++ pages/blogs/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>
+      All the blogs
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default  {
+
+  }
+</script>
