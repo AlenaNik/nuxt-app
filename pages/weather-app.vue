0a348ec557ded6f72b51be7d6135a4a0d531795a
AlenaNik <alena.nik.digital@gmail.com>
2020-02-18 23:33:32 +0100

    generated

--- pages/weather-app.vue
+++ pages/weather-app.vue
@@ -74,21 +74,21 @@
 </template>
 
 <script>
-  import * as am4core from "@amcharts/amcharts4/core";
-  import * as am4charts from "@amcharts/amcharts4/charts";
-  import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-  import am4themes_dark from "@amcharts/amcharts4/themes/dark";
-
-  import Vue from "vue";
-
-  Vue.prototype.$am4core = () => {
-    return {
-      am4core,
-      am4charts,
-      am4themes_animated,
-      am4themes_dark
-    }
-  }
+  // import * as am4core from "@amcharts/amcharts4/core";
+  // import * as am4charts from "@amcharts/amcharts4/charts";
+  // import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+  // import am4themes_dark from "@amcharts/amcharts4/themes/dark";
+  //
+  // import Vue from "vue";
+  //
+  // Vue.prototype.$am4core = () => {
+  //   return {
+  //     am4core,
+  //     am4charts,
+  //     am4themes_animated,
+  //     am4themes_dark
+  //   }
+  // }
   export default {
     data() {
       return {
@@ -99,9 +99,9 @@
     created() {
       this.getWeatherInfo()
     },
-    mounted() {
-      let {am4core, am4charts, am4themes_animated, am4themes_dark} = this.$am4core();
-    },
+    // mounted() {
+    //   let {am4core, am4charts, am4themes_animated, am4themes_dark} = this.$am4core();
+    // },
     computed: {
       icon() {
         return this.weather.weather ? `https://openweathermap.org/img/w/${this.weather.weather[0].icon}.png`
